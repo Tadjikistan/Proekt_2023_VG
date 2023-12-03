382c449c89153e97cb96e970b1ccbf8d6445b22a
xsel <65423939+Tadjikistan@users.noreply.github.com>
2023-12-03 20:59:25 +0300

    +db

--- Discord_bot/bot.py
+++ Discord_bot/bot.py
@@ -1,6 +1,7 @@
 import discord
 import responses
 import pymongo
+
 client = pymongo.MongoClient("mongodb+srv://discordroll:check@discordroll.ej9jzg7.mongodb.net/?retryWrites=true&w=majority")
 db = client.users
 
@@ -10,11 +11,24 @@ async def send_message(message, user_message, is_private):
     try:
         response = responses.get_response(user_message)
         await message.author.send(response) if is_private else await message.channel.send(response)
+        print(db.server_messages_log.find_one({'id': message.author.id}))
+        if db.server_messages_log.find_one({'id': message.author.id}) == None: #Проверка на пользователя в базе данных
+            print(f'New user {message.author} Added')
+            db.server_messages_log.insert_one({"id": message.author.id,"balance": responses.balance})  # Добавляем в коллекцию server_message_log, одну запись
 
-        db.server_messages_log.insert_one({"id": message.author.id,"balance": responses.balance}) #Добавляем в коллекцию server_message_log, одну запись
-
+        else:
+            print(f'Balance of user {message.author} was updated')
+            wallet = db.server_messages_log.find_one({'id': message.author.id}).get('balance')
+            print(type(wallet), 'Счёт')
+            db.server_messages_log.update_one({"id": message.author.id}, {"$set": {"balance":  cashing(wallet, message)}}) #Обновление баланса пользователя
     except Exception as e:
         print(e)
+
+
+def cashing(bal, message: str):
+    print('Im here')
+    bal += 5
+    return bal
 def run_discord_bot():
     TOKEN = ''
     intents = discord.Intents.default()
